404970f628a6c3cbafaeb271ab664618ef0e364c
Charlie Gauthier <31974070+Velythyl@users.noreply.github.com>
2019-01-13 14:25:14 -0500

    Add files via upload

--- parser.pl
+++ parser.pl
@@ -1,23 +1,19 @@
-start(L1, Sugg) :- stat(L1, [], Subsugg), flatten(Subsugg, Sugg).
+start(L1, Sugg) :- stat(L1, [], Sugg).
 
 stat([], Reste, Sugg) :- words("stat", W), stat([W], Reste, Sugg1), append([W], Sugg1, Sugg).
-stat(["if"|L], Reste, Sugg) :- paren_expr(L, R1, Sugg1), paren_expr(R1, R2, Sugg2), Reste=R2, append_all([Sugg1, Sugg2], Sugg).
-stat(["if"|L], Reste, Sugg) :- paren_expr(L, R1, Sugg1), paren_expr(R1, R2, Sugg2), else(R2, R3, Sugg3), Reste=R3, append_all([Sugg1, Sugg2, Sugg3], Sugg).
-stat([X|[]], Reste, Sugg) :- complete("stat", X, W), stat([W], Reste, Sugg1), append([W], Sugg1, Sugg).
+stat(["if"|L], Reste, Sugg) :- paren_expr(L, R1, Sugg1), paren_expr(R1, R2, Sugg2), Reste=R2, append([Sugg1, Sugg2], Sugg).
+stat(["if"|L], Reste, Sugg) :- paren_expr(L, R1, Sugg1), paren_expr(R1, R2, Sugg2), else(R2, R3, Sugg3), Reste=R3, append([Sugg1, Sugg2, Sugg3], Sugg).
+stat(["recurs"|L], Reste, Sugg) :- paren_expr(L, R1, Sugg1), stat(R1, R2, Sugg2), else(R2, R3, Sugg3), Reste=R3, append([Sugg1, Sugg2, Sugg3], Sugg).
+stat([X], Reste, Sugg) :- complete("stat", X, W), stat([W], Reste, Sugg1), append([W], Sugg1, Sugg).
 
 else([], Reste, Sugg) :- words("else", W), else([W], Reste, Sugg1), append([W], Sugg1, Sugg).
 else(["else"|L], Reste, Sugg) :- paren_expr(L, Reste, Sugg).
-else([X|[]], Reste, Sugg) :- complete("else", X, W), else([W], Reste, Sugg1), append([W], Sugg1, Sugg).
+else([X], Reste, Sugg) :- complete("else", X, W), else([W], Reste, Sugg1), append([W], Sugg1, Sugg).
 
-paren_expr([], Reste, Sugg) :- words("paren_expr", W), paren_expr([W], Reste, Sugg1), append_all([W, Sugg1], Sugg). 
+paren_expr([], Reste, Sugg) :- words("paren_expr", W), paren_expr([W], Reste, Sugg1), append([[W], Sugg1], Sugg). 
 paren_expr([X|L], Reste, Sugg) :- words("paren_expr", X), Reste=L, Sugg=[].
-paren_expr([X|[]], Reste, Sugg) :- complete("paren_expr", X, W), paren_expr([W], Reste, Sugg1), append([W], Sugg1, Sugg).
+paren_expr([X], Reste, Sugg) :- complete("paren_expr", X, W), paren_expr([W], Reste, Sugg1), append([W], Sugg1, Sugg).
 
-append_all([[Lone]], [Lone]) :- !.	% Liste imbriquée
-append_all([Lone], [Lone]) :- !.	% Item seul
-append_all([[Subl]|List], Sum) :- append_all(List, Subsum), append([Subl], Subsum, Sum), !.	% Liste imbriquée
-append_all([Ele|List], Sum) :- append_all(List, Subsum), append([Ele], Subsum, Sum), !.		% Item seul
-       
 complete(Type, Deb, W) :- not(words(Type, Deb)), words(Type, Mot), string_length(Deb, Len), sub_string(Mot, Len, _, 0, Apr), string_concat(Deb, Apr, Mot), W=Mot, !.
 
 words("stat", W) :- member(W, ["if"]).	% Ne pas mettre les cas recursif dans ces listes
@@ -31,7 +27,8 @@ words("else", W) :- member(W, ["else"]).
 %append_all( ["allo","salut","bonjour"], Sum).
 %
 %stat: "if" paren_expr paren_expr 
-%        "if" paren_expr paren_expr "else" paren_expr
+%      "if" paren_expr paren_expr "else" paren_expr
+%      "recurs" paren_expr stat "else" paren_expr
 %
 %paren_expr: "paren", "temp", ou "foo"
 %
