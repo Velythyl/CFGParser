0572f8c2008e4747a9d9454cd1895fab46279d52
Charlie Gauthier <31974070+Velythyl@users.noreply.github.com>
2019-01-12 21:33:52 -0500

    Add files via upload

--- parser.pl
+++ parser.pl
@@ -8,15 +8,15 @@ else([], Reste, Sugg) :- else_words(W), else([W], Reste, Sugg1), append([W], Sug
 else(["else"|L], Reste, Sugg) :- paren_expr(L, Reste, Sugg).
 
 paren_expr([], Reste, Sugg) :- paren_expr_words(W), paren_expr([W], Reste, Sugg2), append_all([W, Sugg2], Sugg). 
-paren_expr(["paren"|L], Reste, Sugg) :- Reste=L, Sugg=[].
+paren_expr([X|L], Reste, Sugg) :- paren_expr_words(X), Reste=L, Sugg=[].
 
 append_all([[Lone]], [Lone]) :- !.	% Liste imbriquée
 append_all([Lone], [Lone]) :- !.	% Item seul
 append_all([[Subl]|List], Sum) :- append_all(List, Subsum), append([Subl], Subsum, Sum), !.	% Liste imbriquée
 append_all([Ele|List], Sum) :- append_all(List, Subsum), append([Ele], Subsum, Sum), !.		% Item seul
-
-stat_words(W) :- member(W, ["if", "temp", "foo"]).	% Ne pas mettre les cas recursif dans ces listes
-paren_expr_words(W) :- member(W, ["paren"]).
+       
+stat_words(W) :- member(W, ["if"]).	% Ne pas mettre les cas recursif dans ces listes
+paren_expr_words(W) :- member(W, ["paren", "temp", "foo"]).
 else_words(W) :- member(W, ["else"]).
 
 %start(["if", "paren", "paren", "else"], Sugg).
@@ -24,3 +24,18 @@ else_words(W) :- member(W, ["else"]).
 %append_all( ([["allo"],["salut"],["bonjour"]]), Sum).
 %
 %append_all( ["allo","salut","bonjour"], Sum).
+%
+%stat: "if" paren_expr paren_expr 
+%        "if" paren_expr paren_expr "else" paren_expr
+%
+%paren_expr: "paren", "temp", ou "foo"
+%
+%Chaque type a son cas vide, de forme type([], Reste, Sugg). Ceci est le traitement de suggestion: on génère les mots possible pour le type, on appelle le type sur chacun de ses mots, et on ajout le mot aux Sugg.
+%Chaque type a ses cas non-vides, de forme type([X|L], Reste, Sugg). Ceci est le traitement de phrase déjà écrite. 
+%
+%Le cas vide ne PEUT PAS traiter de cas récursifs (autrement dit, on ne peut mettre des cas récursifs dans la liste de mots du type), puisque prolog entrera dans une boucle infinie.
+%Le cas non-vide peut avoir deux formes: 
+%	Soit X est un littéral placé dans la tête de la déclaration du type, comme type(["mot"|L], Reste, Sugg). Ceci ne laisse place qu'à une seule option au début du type.
+%	Soit X est vérifié dans le corps de la déclaration par un appel à type_words(X). Ceci laisse place à plusieurs options en début de type.
+%	
+%	On pourrait traiter le premier cas avec la méthode du deuxième, mais cette façon de faire évite des appels de fonctions et rend la lecture humaine plus rapide.
