3d54fdb9da01a76d31070e8423cfa5927f3e7a4d
Charlie Gauthier <31974070+Velythyl@users.noreply.github.com>
2019-01-12 21:09:17 -0500

    Add files via upload

--- parser.pl
+++ parser.pl
@@ -1,11 +1,11 @@
 start(L1, Sugg) :- stat(L1, [], Subsugg), flatten(Subsugg, Sugg).
 stat([], Reste, Sugg) :- stat_words(W), stat([W], Reste, Sugg1), append([W], Sugg1, Sugg).
-stat([X|L], Reste, Sugg) :- X="if", paren_expr(L, R1, Sugg1), paren_expr(R1, R2, Sugg2), Reste=R2, append_all([Sugg1, Sugg2], Sugg).
-stat([X|L], Reste, Sugg) :- X="if", paren_expr(L, R1, Sugg1), paren_expr(R1, R2, Sugg2), else(R2, R3, Sugg3), Reste=R3, append_all([Sugg1, Sugg2, Sugg3], Sugg).
+stat(["if"|L], Reste, Sugg) :- paren_expr(L, R1, Sugg1), paren_expr(R1, R2, Sugg2), Reste=R2, append_all([Sugg1, Sugg2], Sugg).
+stat(["if"|L], Reste, Sugg) :- paren_expr(L, R1, Sugg1), paren_expr(R1, R2, Sugg2), else(R2, R3, Sugg3), Reste=R3, append_all([Sugg1, Sugg2, Sugg3], Sugg).
 else([], Reste, Sugg) :- else_words(W), else([W], Reste, Sugg1), append([W], Sugg1, Sugg).
-else([X|L], Reste, Sugg) :- X="else", paren_expr(L, Reste, Sugg).
+else(["else"|L], Reste, Sugg) :- paren_expr(L, Reste, Sugg).
 paren_expr([], Reste, Sugg) :- paren_expr_words(W), paren_expr([W], Reste, Sugg2), append_all([W, Sugg2], Sugg). 
-paren_expr([X|L], Reste, Sugg) :- X="paren", Reste=L, Sugg=[].
+paren_expr(["paren"|L], Reste, Sugg) :- Reste=L, Sugg=[].
 
 append_all([[Lone]], [Lone]) :- !.
 append_all([Lone], [Lone]) :- !.
